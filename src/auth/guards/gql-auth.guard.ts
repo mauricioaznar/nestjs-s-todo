3f6412940765ee759ef5304623891ebc57f1c72e
mauricioaznar <mauricioaznar94@gmail.com>
2021-10-22 09:50:07 -0500

    ssdsas

--- src/auth/guards/gql-auth.guard.ts
+++ src/auth/guards/gql-auth.guard.ts
@@ -5,7 +5,6 @@ export class GqlAuthGuard extends AuthGuard('jwt') {
   getRequest(context) {
     // req used in http queries and mutations, connection is used in websocket subscription connections, check AppModule
     const ctx = GqlExecutionContext.create(context).getContext();
-    console.log(ctx.connectionParams);
     return ctx.connectionParams ? { headers: ctx.connectionParams } : ctx.req;
   }
 }
